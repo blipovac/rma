051cc337bab3e125de57a39b299f5726289bb886
ivansertic <serticivan5@gmail.com>
2021-05-13 21:29:09 +0200

    feat: create user data class

---
+++ app/src/main/java/com/example/bruno_lipovac_rma/models/User.kt
@@ -0,0 +1,5 @@
+package com.example.bruno_lipovac_rma.models
+
+import com.example.bruno_lipovac_rma.models.enums.UserType
+
+data class User(val email: String, val password: String, val userType: UserType)
