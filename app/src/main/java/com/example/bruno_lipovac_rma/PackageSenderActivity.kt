0b16f9e1739a622d4d04a199703c55943b9694df
brunolipovac <othermmo@gmail.com>
2021-06-09 21:46:42 +0200

    feat: create basic courier/sender activities

---
+++ app/src/main/java/com/example/bruno_lipovac_rma/PackageSenderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bruno_lipovac_rma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PackageSenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_package_sender)
+    }
+}
