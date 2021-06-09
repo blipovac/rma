c6ad89e10f6cb89b89421ef31f474a10c7a91374
brunolipovac <othermmo@gmail.com>
2021-06-09 21:48:12 +0200

    feat: WIP login

--- app/src/main/java/com/example/bruno_lipovac_rma/MainActivity.kt
+++ app/src/main/java/com/example/bruno_lipovac_rma/MainActivity.kt
@@ -6,7 +6,10 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bruno_lipovac_rma.databinding.ActivityMainBinding
+import com.example.bruno_lipovac_rma.models.User
+import com.example.bruno_lipovac_rma.models.enums.UserType
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 
 
@@ -60,6 +63,11 @@ class MainActivity : AppCompatActivity() {
         ).addOnCompleteListener(this) { task ->
             if (task.isSuccessful) {
                 Log.d("AUTH", "login is successful")
+
+
+                val authUser = auth.currentUser
+
+                this.getUser(authUser)
             } else {
                 Log.d("AUTH", "login failed")
 
@@ -67,5 +75,47 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
         }
+    }
+
+    private fun goToSomeScreen(user: User?) {
+        when (user?.userType) {
+            UserType.COURIER -> {
+                val intent = Intent(this, PackageCourierActivity::class.java)
+
+                startActivity(intent)
+            }
+            UserType.SENDER -> {
+                val intent = Intent(this, PackageSenderActivity::class.java)
+
+                startActivity(intent)
+            }
+            else -> {
+                val intent = Intent(this, MainActivity::class.java)
+
+                Toast.makeText(baseContext, "Some error occurred", Toast.LENGTH_SHORT).show()
+
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun getUser(authUser: FirebaseUser?) {
+        db.collection("users").whereEqualTo("uid", authUser).get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val user = User(
+                        document.data["email"].toString(),
+                        document.data["password"].toString(),
+                        document.data["userType"] as UserType,
+                        document.data["uid"].toString()
+
+                    )
+
+                    this.goToSomeScreen(user)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("USER_FETCH", "user fetch failed: ", exception)
+            }
     }
 }
