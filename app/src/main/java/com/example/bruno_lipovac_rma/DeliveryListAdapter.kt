15b12d383aa375ec244d78129cb69fe6d09d2568
brunolipovac <othermmo@gmail.com>
2021-08-28 11:11:12 +0200

    feat: add on click listeners to list items DeliveryListAdapter.kt

--- app/src/main/java/com/example/bruno_lipovac_rma/DeliveryListAdapter.kt
+++ app/src/main/java/com/example/bruno_lipovac_rma/DeliveryListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.bruno_lipovac_rma
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,24 +10,39 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bruno_lipovac_rma.models.Delivery
 
-class DeliveryListAdapter():
+class DeliveryListAdapter(private val onClick: (Delivery) -> Unit):
     ListAdapter<Delivery, DeliveryListAdapter.ViewHolder>(DeliveryDiffCallback) {
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, val onClick: (Delivery) -> Unit): RecyclerView.ViewHolder(view) {
         val pickupTextView: TextView = view.findViewById(R.id.recycler_pickup_address)
         val deliveryTextView: TextView = view.findViewById(R.id.recycler_delivery_address)
+        var currentDelivery: Delivery? = null
+
+        init {
+            view.setOnClickListener {
+                Log.d("LIST_ITEM", "CLICKED!")
+                currentDelivery?.let {
+                    onClick(it)
+                }
+            }
+        }
+
+        fun bind(delivery: Delivery) {
+            currentDelivery = delivery
+            pickupTextView.text = delivery.pickupAddress
+            deliveryTextView.text = delivery.deliverAddress
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.delivery_list_item, parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder(view, onClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val delivery = getItem(position)
-        holder.pickupTextView.text = delivery.pickupAddress
-        holder.deliveryTextView.text = delivery.deliverAddress
+        holder.bind(delivery)
     }
 }
 
